c83b801d815481a253ad026fbf3d990cbfb8eed7
Kentaro Matsuzaki <kentaro0919@gmail.com>
2018-03-31 11:10:40 +0900

    Pasona lab for Python 2018-03-31 OOP with python -First commit

---
+++ hello.py
@@ -0,0 +1,33 @@
+# coding:utf-8
+""""
+print("Hello, ", end="")
+print("World")
+
+print(help(print))
+"""
+
+#print(input("What would you like to do?: ")) # ユーザーへ何がしたいのかをプロンプト
+
+print(
+    'gido', "guido", '''guido''', """guido"""
+)
+
+print("hello, " + "world")
+print("hello, " * 5)
+print(
+    2 + 5 ,
+    2-5,
+    2*5,
+    2/5,
+    8%3,
+    5 ** 2,
+    9// 2,
+)
+
+print(
+    "5"+"9",
+    int('5')+int("99"),
+    5+99,
+    str(5)+str(99)
+)
+
